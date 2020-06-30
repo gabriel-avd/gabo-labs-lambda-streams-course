927c270f8a667dab6dc60f1483ecd69bdc02a958
Gabriel Valdes <gabriel_avd@hotmail.com>
2020-06-29 22:22:51 -0700

    Added functional interfaces package

--- src/com/gabo/labs/functional/interfaces/Main.java
+++ src/com/gabo/labs/functional/interfaces/Main.java
@@ -1,15 +1,27 @@
 package com.gabo.labs.functional.interfaces;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Predicate<String> pred = s -> s.length() < 20;
-        System.out.println(pred.test("Hola mundossssssssssssssssss"));
+        Predicate<String> pred1 = s -> s.length() < 20;
+        Predicate<String> pred2 = s -> s.length() > 5;
+        System.out.println(pred1.test("Hola mundossssssssssssssssss"));
+
+        Predicate<String> pred3 =  pred1.and(pred2);
+        Predicate<String> pred4 =  pred1.or(pred2);
+        System.out.println("\nPred 3: Hola mundo -"+pred3.test("Hola mundo"));
+        System.out.println("Pred 3: Hol -"+pred3.test("Hol"));
+
+        System.out.println("\nPred 4: Hola mundossssssssssssssssss -"+pred4.test("Hola mundossssssssssssssssss"));
+        System.out.println("Pred 4: Hol -"+pred4.test("Hol"));
+
+        BiPredicate <Integer, Integer> biPredicate = (x,y) -> (x + y ) > 50;
+
+        System.out.println("\nBiPredicate : "+biPredicate.test(15,50));
 
         Consumer<String> stringConsumer = System.out::println;
         stringConsumer.accept("Hola Mundo");
